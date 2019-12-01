956236d30ed9d452ec04cdf411e4d22400e91c6f
princelewis <princelewis4christ@yahoo.com>
2019-12-01 20:49:14 +0100

    create the createZip method

--- src/com/FileHandlers.java
+++ src/com/FileHandlers.java
@@ -1,4 +1,32 @@
 package com;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class FileHandlers {
+
+    //create a method to create zip file
+    public FileSystem createZip(Path fileAsPath) throws URISyntaxException, IOException {
+
+        //create sort of a permission that would be needed for creating a zip file
+        Map<String, String> providerProps = new HashMap<>() {{
+           put("create", "true");
+        }};
+
+        //generate a URI for the zip file to be created. The URI is suppose to start from the root folder to this current directory
+        URI zipURI = new URI("jar:file",fileAsPath.toUri().getPath(), null);
+
+        //using the filesystem method, create the zip file
+        FileSystem createdZip = FileSystems.newFileSystem(zipURI, providerProps);
+
+        return createdZip;
+    }
 }
